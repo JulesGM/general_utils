832f0e7c94347bc70772994959da5a1801695b9d
Jules Gagnon-Marchand <jules.gagnonm.alt@gmail.com>
2022-09-06 16:50:28 -0400

    small fixes

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="general_utils",
+      version="1.0",
+      description="@julesgm's general Python utilities",
+      author="Jules Gagnon-Marchand",
+      author_email="jgagnonmarchand@gmail.com",
+      packages=["general_utils"],
+      package_dir = {'': 'src'},
+     )
